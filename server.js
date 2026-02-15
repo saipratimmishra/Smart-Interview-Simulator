256ac2bdff4dbf3615ada05ca67fce4d49ff9b51
Sai Pratim Mishra <164091269+saipratimmishra@users.noreply.github.com>
2026-02-15 23:54:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+let questions = [];
+const Groq = require("groq-sdk");
+
+let question=undefined;
+let quesanswer=undefined;
+let sub;
+let diff;
+const client = new Groq({
+   apiKey: "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"
+});
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    async function generateQuestion(socket, subject){
+        console.log(subject)
+        difficulty=subject.difficulty
+        subject=subject.role
+        
+        console.log(subject,difficulty)
+        try{
+        const chatCompletion = await client.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            messages: [
+                {
+                    role: "system",
+                    content: `
+                    You are a senior technical interviewer.
+                    Subject interpretation rules:
+
+                    - If subject is "frontend":
+                    - web frontend development (HTML, CSS, JavaScript, DOM, React concepts, browser behavior).
+
+                    - If subject is "backend":
+                    - server-side development (Node.js, APIs, databases, authentication, system design).
+
+                    - If subject is "java":
+                    - Java programming language, OOP, collections, multithreading, JVM concepts.
+
+                    - If subject is "python":
+                    - Python programming, data structures, functions, async, libraries.
+
+                    - If subject is "anatomy":
+                    - HUMAN ANATOMY (biology/medical), organs, body systems, physiology.
+
+                    
+                    Rules:
+                    - Ask ONE question only not more than 2 sentence.
+                    - Do not repeat previous questions.
+                    - Do not give answer
+                    `
+                },
+                {
+                    role: "user",
+                    content: `Ask a ${difficulty} ${subject} interview question.
+                            Avoid questions from this as well as related to this: ${JSON.stringify(questions)}`
+                }
+            ]
+        });
+        let bot_reply = chatCompletion.choices[0].message.content;
+        question = bot_reply;
+        socket.emit("question", bot_reply);
+        }
+        catch(exception){
+            console.log(exception)
+        }
+    }
+
+    console.log("User connected ");
+    socket.on("startInterview",async (subject,difficulty)=>{
+        questions=[]
+        sub=subject
+        diff=difficulty
+        await generateQuestion(socket, subject, difficulty);
+    })
+
+    socket.on("Answer",async (answer)=>{
+        quesanswer=answer
+        questions.push({question:question, answer:quesanswer})
+        await generateQuestion(socket, sub, diff);
+        console.log(questions)
+    });
+    
+    socket.on("feedback",async ()=>{
+        
+        const chatCompletion = await client.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            messages: [
+                {
+                    role: "system",
+                    content: `You are a senior technical interviewer evaluating candidate answers.
+                        Subject interpretation rules:
+
+                        - frontend : HTML, CSS, JavaScript, DOM, browser concepts
+                        - backend : APIs, databases, server architecture
+                        - java : Java language, OOP, JVM
+                        - python : Python programming
+                        - anatomy : HUMAN ANATOMY (medical/biology)
+
+                        Evaluation Rules:
+                        - Be honest and professional.
+                        - Give constructive feedback.
+                        - Mention strengths.
+                        - Mention mistakes or missing concepts.
+                        - Suggest improvements.
+                        - DO NOT generate new questions.
+                        - Keep feedback concise and clear.
+
+                        Output format:
+
+                        Strengths:(be strict dont mention if it is not there or answer is irrelivant)
+                        Weaknesses:
+                        Improvements:
+                        Overall rating (1-10):
+                    `
+                },
+                {
+                    role: "user",
+                    content: `Evaluate candidate answers based on the following interview history: ${JSON.stringify(questions,null,2)}`
+                }
+            ]
+        });
+        let bot_reply = chatCompletion.choices[0].message.content;
+        console.log(bot_reply)
+        socket.emit("feedback",bot_reply)
+    })
+
+})
+
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
